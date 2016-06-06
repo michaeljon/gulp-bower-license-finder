4483a742b19ad5ac2bc4a86636627026289744b1
liront <liront@C-B4.com>
2016-06-06 13:43:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var gutil = require('gulp-util');
+var path = require('path');
+var Stream = require('stream');
+var license = require('bower-license');
+
+var PLUGIN_NAME = 'gulp-bower-license-finder';
+
+function bowerLicenseFinder(filename, options) {
+	filename = filename || 'bowerLicenses.txt';
+	options = options || {};
+	options.path = options.path || './www/lib';
+
+	var stream = new Stream();
+
+	license.init(options.path, function(licenseMap, err){
+		if (err) {
+			throw new gutil.PluginError(PLUGIN_NAME, err, {showStack: true});
+		}
+
+		var file = new gutil.File({
+			path: path.join(process.cwd(), filename),
+			contents: new Buffer(JSON.stringify(licenseMap))
+		});
+
+		stream.emit('data', file);
+		stream.emit('finish');
+	});
+
+	return stream;
+}
+
+module.exports = bowerLicenseFinder;

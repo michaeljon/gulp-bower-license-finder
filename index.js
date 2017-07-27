580f79e553a97a75d233058c155cffb5edd723c5
Michaeljon Miller <michaeljon.miller@outlook.com>
2017-07-27 15:53:08 -0700

    fixing path issues

--- index.js
+++ index.js
@@ -6,27 +6,27 @@ var license = require('bower-license');
 var PLUGIN_NAME = 'gulp-bower-license-finder';
 
 function bowerLicenseFinder(filename, options) {
-	filename = filename || 'bowerLicenses.txt';
-	options = options || {};
-	options.path = options.path || './www/lib';
+  filename = filename || 'bowerLicenses.txt';
+  options = options || {};
+  options.path = options.path || './www/lib';
 
-	var stream = new Stream();
+  var stream = new Stream();
 
-	license.init(options.path, function(licenseMap, err){
-		if (err) {
-			throw new gutil.PluginError(PLUGIN_NAME, err, {showStack: true});
-		}
+  license.init(options, function (licenseMap, err) {
+    if (err) {
+      throw new gutil.PluginError(PLUGIN_NAME, err, { showStack: true });
+    }
 
-		var file = new gutil.File({
-			path: path.join(process.cwd(), filename),
-			contents: new Buffer(JSON.stringify(licenseMap))
-		});
+    var file = new gutil.File({
+      path: path.join(process.cwd(), options.path, filename),
+      contents: new Buffer(JSON.stringify(licenseMap))
+    });
 
-		stream.emit('data', file);
-		stream.emit('finish');
-	});
+    stream.emit('data', file);
+    stream.emit('finish');
+  });
 
-	return stream;
+  return stream;
 }
 
 module.exports = bowerLicenseFinder;
